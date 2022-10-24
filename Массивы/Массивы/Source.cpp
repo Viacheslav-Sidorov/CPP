d2e470ea76848a23fd09f8d9deeb7078036dc318
Андрей Боев <a.boev.it@gmail.com>
2022-10-24 18:03:03 +0300

    Has been added Массивы

---
+++ Массивы/Массивы/Source.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+
+void FillRandom(int* array, int size)
+{
+	for (int i = 0; i < size; i++)
+		array[i] = rand() % 100;
+}
+
+void PrintArray(int* array, int size)
+{
+	for (int i = 0; i < size; i++)
+		std::cout << array[i] << ' ';
+	std::cout << std::endl;
+}
+
+int main()
+{
+	// Указатель на целый тип.
+	int* arr;
+	int size;
+	std::cin >> size;
+	// Выделение памяти для массива
+	arr = new int[size];
+	srand(time(0));
+	FillRandom(arr, size);
+	PrintArray(arr, size);
+
+	return 0;
+}
